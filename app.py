d9ae5190177985ba78fbd14fa15c8709d9368192
cristel2027 <24-32901@g.batstate-u.edu.ph>
2025-12-08 17:22:49 +0800

    initial setup

--- app.py
+++ app.py
@@ -13,5 +13,26 @@ app.config['WTF_CSRF_ENABLED'] = False
 
 db.init_app(app)
 
+# ---------------- BASIC PAGES ----------------
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/view')
+def view():
+    data = {
+       "users": User.query.all(),
+       "terminals": Terminal.query.all(),
+       "jeepneys": Jeepney.query.all(),
+       "terminal_queue": TerminalJeepneys.query.all(),
+       "trips": Trip.query.all(),
+       "seats": Seat.query.all(),
+       "routes": Route.query.all(),
+       "favorites": Userfavorite.query.all(),
+       "notifications": Notification.query.all(),
+       "auditlogs": Auditlog.query.all(),
+   }
+    return render_template("view_database.html", data=data) 
+
 if __name__ == "__main__":
   app.run(debug=True)
