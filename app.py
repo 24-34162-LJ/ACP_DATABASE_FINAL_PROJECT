69d27288da0d704ef7d1d458a6996afb6a0e5904
cristel2027 <24-32901@g.batstate-u.edu.ph>
2025-12-08 19:08:29 +0800

    commit

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template, redirect, url_for, session, request, flash
+from flask import Flask, render_template, redirect, url_for, session, request, flash, jsonify, current_app, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import RegisterForm, LoginForm, AddTerminal
-from models import db, User, Terminal
+from models import (
+    db, User, Terminal, Jeepney, Trip, Seat, TerminalJeepneys,
+    Route, Auditlog, Userfavorite, Notification
+)
+
+from forms import (
+    UserForm, TerminalForm, RouteForm, JeepneyForm,
+    TripForm, SeatForm, TerminalJeepneysForm,
+    UserfavoriteForm, NotificationForm, AuditlogForm
+)
+
+from datetime import datetime
+from sqlalchemy import func
+
+# ---------------- GLOBAL CONSTANTS ----------------
+MAIN_TERMINAL_ID = 1
 
 # ---------------- FLASK APP SETUP ----------------
 app = Flask(__name__)
