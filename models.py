e7067e2eb7e6130af55e4622e141ca64b8eeb8c3
cristel2027 <24-32901@g.batstate-u.edu.ph>
2025-12-12 17:52:20 +0800

    terminal

--- models.py
+++ models.py
@@ -58,3 +58,64 @@ class User(db.Model,  UserMixin):
 
     def __repr__(self):
         return f"<User {self.first_name} {self.last_name}>"
+    
+class Terminal(db.Model):
+
+    __tablename__ = "terminals"
+
+    terminal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    terminal_name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(150), nullable=False)
+
+    status = db.Column(
+        db.Enum('active', 'inactive', name='terminal_status'),
+        nullable=False,
+        default='active'
+    )
+    
+    is_main = db.Column(db.Boolean, default=False, nullable=False)
+
+    # to relationship
+
+    origin_route = db.relationship(
+        "Route",
+        foreign_keys='Route.start_terminal_id',
+        back_populates="start_terminal"
+    )
+
+    destination_route = db.relationship(
+        "Route",
+        foreign_keys='Route.end_terminal_id',
+        back_populates="end_terminal"
+    )
+
+    origin_trip_pk = db.relationship(
+        "Trip",
+        foreign_keys='Trip.origin_terminal_id',
+        back_populates="origin_fk"
+    )
+
+    destination_trip_pk = db.relationship(
+        "Trip",
+        foreign_keys='Trip.destination_terminal_id',
+        back_populates="destination_fk"
+    )
+
+    terminal_jeep_pk = db.relationship(
+        "TerminalJeepneys",
+        foreign_keys='TerminalJeepneys.terminal_id',
+        back_populates='terminal_jeep_fk',
+        cascade="all, delete-orphan",
+        passive_deletes=True
+    )
+
+    favorite_terminal_pk = db.relationship(
+        "Userfavorite",
+        foreign_keys='Userfavorite.terminal_id',
+        back_populates='favorite_terminal_fk',
+        cascade="all, delete-orphan",
+        passive_deletes=True
+    )
+
+    def __repr__(self):
+        return f"<terminal {self.terminal_name}>"
