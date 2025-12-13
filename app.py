ed32b1e8321bd72e05302d0ee096bd0e0c22617b
24-36772-Maranan <24-36772@g.batstate-u.edu.ph>
2025-12-13 21:17:47 +0800

    flask app

--- app.py
+++ app.py
@@ -21,10 +21,13 @@ import os
 MAIN_TERMINAL_ID = 1
 
 # ---------------- FLASK APP SETUP ----------------
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+os.makedirs(app.instance_path, exist_ok=True)
+
+db_path = os.path.join(app.instance_path, 'jeepbapa.db')
 
 app.config['SECRET_KEY'] = 'lj123'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jeep.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path.replace('\\','/')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['WTF_CSRF_ENABLED'] = False
 
